3ff93181bf1cc4107dbd459ab8505b4f75c5ad13
prakash7007-gp <bcaprakash117@gmail.com>
2024-09-03 22:31:02 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,8 @@
+#value input in squre brackets
+# its duplicates allowed
+#insertion order to output
+# index based working
+# method
+#1 append    is  adding value in last
+#2 insert    is  whether it user want to insert any place\
+# 3 extend  is assign must squre baracket
